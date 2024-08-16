e0cd21c2a56d99cb17363b2b87d0cedca448fee1
InshutiSouvede <isouvede2@gmail.com>
2024-08-16 14:28:25 +0200

    Display by category

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,16 +5,17 @@ import ExpensesFilter from './ExpensesFilter';
 export default function Home() {
     const [expenses,setExpenses] = useState([
         {id:1,description:"aaa",amount:10,category:"Utilities"},
-        {id:2,description:"aaa",amount:10,category:"Utilities"},
-        {id:3,description:"aaa",amount:10,category:"Utilities"},
+        {id:2,description:"aaa",amount:10,category:"Groceries"},
+        {id:3,description:"aaa",amount:10,category:"Entertainment"},
         {id:4,description:"aaa",amount:10,category:"Utilities"}
     ])
+    const [selectedCategory,setSelectedCategory] = useState<string>("")
     return (
         <div className='px-20 text-2xl text-cyan-900'>
             <Form />
-            <ExpensesFilter onSelectCategory={(category)=>console.log(category)} />
+            <ExpensesFilter onSelectCategory={(category)=>setSelectedCategory(category)} />
             <ExpenseList 
-            expenses={expenses}
+            expenses={expenses.filter((el)=>!selectedCategory?true:el.category==selectedCategory)}
             onDelete={(id)=>setExpenses(expenses.filter((expense)=>expense.id!=id))}
             />
         </div>
