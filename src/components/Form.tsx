1d59a0e6db8996c8aa6fafc3dea36cde6c499ffd
InshutiSouvede <isouvede2@gmail.com>
2024-08-16 14:45:20 +0200

    Form for expenses

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,11 @@
 import {FieldValues, useForm } from "react-hook-form";
 import {z} from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { categories } from "./Home";
 const schema =  z.object({
     name: z.string().min(3,{message:"Name must be atleast 3 characters"}),
-    age: z.number({message:"Age field is required"}).min(18,{message:"Age must be atleast 18"})
+    category: z.string(),
+    amount: z.number({message:"Age field is required"}).min(18,{message:"Age must be atleast 18"})
 });
 type FormData = z.infer<typeof schema>
 export default function Form() {
@@ -20,11 +22,17 @@ export default function Form() {
                 {errors.name&&<p className="text-red-600">{errors.name.message}</p>}                
             </div>
             <div className="flex flex-col gap-3">                
-                <label htmlFor="age">Age</label>
-                <input  type="number" id="age" {...register('age',{valueAsNumber:true})} className="border rounded-md h-14 px-5" />
-                {errors.age&&<p className="text-red-600">{errors.age.message}</p>}
+                <label htmlFor="amount">Amount</label>
+                <input  type="number" id="amount" {...register('amount',{valueAsNumber:true})} className="border rounded-md h-14 px-5" />
+                {errors.amount&&<p className="text-red-600">{errors.amount.message}</p>}
             </div>
+            <label htmlFor="category">Category</label>
+            <select className="w-full h-14 rounded-md px-5 border-2 bg-transparent" id="category" {...register('category',{valueAsNumber:true})}>
+            <option value=""></option>
+            {categories.map((category)=><option value={category}>{category}</option>)}
+           
+            </select>
             <button disabled={!isValid} type="submit" className={`w-max px-5 h-14 rounded-md ${isValid?"bg-cyan-900":"bg-gray-600"}  text-white`}>Submit</button>
         </form>
-    );
+    )
 }
